f7d35925f5d9142c9234f76e127a02169867c9d3
Zaw Lin Htet <zawlin225533@gmail.com>
2025-06-09 11:42:40 +0630

    Initial commit

--- resources/views/articles/career_detail.blade.php
+++ resources/views/articles/career_detail.blade.php
@@ -1,34 +1,33 @@
 @extends('layouts.app')
 
-@section('content') 
-    <div class="container">
-        <div class="row">
-            <div class="col-5 mt-4">
-                <p class="text-muted">{{ $jobPost->created_at->format('M d, Y') }} | Career</p>
-                <h1>{{$jobPost->title}}</h1>
-            </div>
-            <div class="col-7 mt-5">
-                <div>
-                    <h4>Description</h4>
-                    <p class="w-75 fs-4">{{$jobPost->description}}</p>
-                </div>
-                <div>
-                    <h4 class="mt-3">Job Requirements</h4>
-                    <p class="w-75 fs-4">{{$jobPost->job_requirements}}</p>
-                </div>
-                <div>
-                    <h4 class="mt-3">Other</h4>
-                    <p class="w-75 fs-4">{{$jobPost->job_requirements}}</p>
-                </div>
-                <div>
-                    <p class="fs-4">admin.hr@winthinassociates.com</p>
-                </div>
-            </div>
+@section('content')
+<div class="container py-5" style="min-height: 55vh;">
+    <div class="row">
+        <div class="col-lg-6 mb-4">
+            <p class="text-muted">
+                {{ $jobPost->created_at->format('M d, Y') }} &bull; <strong>Career</strong>
+            </p>
+            <h2 class="fw-bold">{{ $jobPost->title }}</h2>
         </div>
-        <div class="mt-4">
-            <a href="{{ route('articles.career') }}" class="btn btn-outline-primary">
+    </div>
+
+    <div class="row">
+        <div class="col-lg-10">
+            <div class="mb-4">
+                <h4>Description</h4>
+                <p class="fs-5">{{ $jobPost->description ?? $jobPost->body }}</p>
+            </div>
+
+            <div class="mb-4">
+                <h5>Email your application to:</h5>
+                <p class="fs-5">admin.hr@winthinassociates.com </p>
+                
+            </div>
+
+            <a href="{{ route('admin.careers') }}" class="btn btn-outline-primary">
                 ← Back to All Posts
             </a>
         </div>
     </div>
+</div>
 @endsection
